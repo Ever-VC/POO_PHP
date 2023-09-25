ebbc1f5fd000893ca17b5f8bcb0f56ef3c4f675f
Ever-VC <ever.vasquezc27@gmail.com>
2023-09-24 18:44:42 -0600

    Commit inicial, contiene la estructura básica del MVC, el header, footer y un apartado main de prueba

---
+++ views/footer.php
@@ -0,0 +1,31 @@
+    <footer>
+        <div class="group-1">
+            <div class="box">
+                <figure>
+                    <a href="#">
+                        <img src="<?= BASE_DIR; ?>imgs/logotipo-sleedw.svg" alt="logotipo">
+                    </a>
+                </figure>
+            </div>
+            <div class="box">
+                <h2>SOBRE NOSOTROS</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, reprehenderit?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, reprehenderit?</p>
+            </div>
+            <div class="box">
+                <h2>SIGUEMOS</h2>
+                <div class="social">
+                    <a href="#" class="fa-brands fa-facebook"></a>
+                    <a href="#" class="fa-brands fa-instagram"></a>
+                    <a href="#" class="fa-brands fa-twitter"></a>
+                    <a href="#" class="fa-brands fa-youtube"></a>
+                </div>
+            </div>
+        </div>
+        <div class="group-2">
+            <small>&copy;2023 <b>Ever-VC</b> - Todos los Derechos Reservados</small>
+        </div>
+    </footer>
+</body>
+</html>
+<script src="<?= BASE_DIR; ?>/js/main.js" type="module"></script>
---
+++ views/home.php
@@ -0,0 +1,9 @@
+    <main>
+        <section id="hero">
+            <h1>Welcome</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur. <br>
+                Hic sunt dolorem incidunt quae debitis.
+            </p>
+        </section>
+    </main>
